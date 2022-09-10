b69412cb6179235bf00668898a10025799121c44
MaxaiZer <MaxuZerGames@gmail.com>
2022-09-10 13:57:35 +0700

    cycle optimization

--- OpenMP/lab1.c
+++ OpenMP/lab1.c
@@ -20,13 +20,13 @@ int* getPrimeNumbers(int count)
 
     for (int n = 3; curCount != count; n += 2)
     {
-         bool isPrime = true;
-      //  #pragma omp parallel for if (isParallel && curCount > 100) shared(isPrime, n)
+        bool isPrime = true;
+        //  #pragma omp parallel for if (isParallel && curCount > 100) shared(isPrime, n)
         for (int i = 1; i < curCount; i++)
         {
             if (n % primeNumbers[i] == 0)
             {
-            //    #pragma omp critical
+                //    #pragma omp critical
                 isPrime = false;
             }
         }
@@ -51,39 +51,45 @@ struct Result solveTask(int number)
 
     struct Result result = {INT_MAX, {-1, -1, -1, -1}};
 
-#pragma omp parallel for collapse(3) if (isParallel) shared(number, primeNumbersCount, primeNumbers)
+    #pragma omp parallel for collapse(3) if (isParallel) shared(number, primeNumbersCount, primeNumbers)
     for (int i = 0; i < primeNumbersCount; i++)
     {
         for (int j = 0; j < primeNumbersCount; j++)
         {
             for (int k = 0; k < primeNumbersCount; k++)
-            {  
+            {
                 struct Result threadResult = {INT_MAX, {-1, -1, -1, -1}};
-            
+                bool skipCycle = false;
+
                 for (int s = 0; s < primeNumbersCount; s++)
                 {
 
-                    if (i == j || i == k || i == s || j == k || j == s || k == s)
+                    if (i == j || i == k || i == s || j == k || j == s || k == s || skipCycle)
                         continue;
 
                     int n = pow(primeNumbers[i], 2) + pow(primeNumbers[j], 3) +
                             pow(primeNumbers[k], 4) + pow(primeNumbers[s], 5);
-                
-                    if (n > number && n < threadResult.number)
+
+                    if (n > number)
                     {
-                        struct Result _result =
-                            {
-                                n,
-                                {primeNumbers[i], primeNumbers[j],
-                                 primeNumbers[k], primeNumbers[s]}
-                            };
-                        threadResult = _result;
+                        if (n < threadResult.number)
+                        {
+                            struct Result _result =
+                                {
+                                    n,
+                                    {primeNumbers[i], primeNumbers[j],
+                                     primeNumbers[k], primeNumbers[s]}
+                                };
+                            threadResult = _result;
+                        }
+                        else
+                            skipCycle = true;
                     }
                 }
-                
-                    #pragma omp critical
+
+                #pragma omp critical
                 if (threadResult.number < result.number)
-                {               
+                {
                     result = threadResult;
                 }
             }
@@ -115,7 +121,7 @@ int main(int argc, char **argv)
     clock_gettime(CLOCK_REALTIME, &end);
 
     printf("%d = %d^2 + %d^3 + %d^4 + %d^5\n", res.number, res.primeNumbers[0], res.primeNumbers[1], res.primeNumbers[2], res.primeNumbers[3]);
-    printf("Time: %.6f sec\n", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1.0 / 1000000000);
+    printf("time: %.6f sec\n", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1.0 / 1000000000);
 
     return 0;
 }
