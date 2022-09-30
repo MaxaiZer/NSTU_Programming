a6b5c093b99fcd01a23c153a4c2546fc1241e053
MaxaiZer <MaxuZerGames@gmail.com>
2022-09-30 21:26:52 +0700

    small fix

--- Cuda/main.cpp
+++ Cuda/main.cpp
@@ -3,6 +3,7 @@
 #include <limits.h>
 #include <ctime> 
 #include <stdexcept>
+#include <string>
 #include "task11.h"
 
 struct ExecutionInfo
@@ -11,13 +12,13 @@ struct ExecutionInfo
 	float time; //sec
 };
 
-void generateMatrix(int** matrix, int rows, int columns, int min, int max)
+void generateMatrix(long long** matrix, int rows, int columns, int min, int max)
 {
 	if (rows <= 0 || columns <= 0) throw std::invalid_argument::exception();
 
 	srand(time(0));
 
-	*matrix = (int*)malloc(rows * columns * sizeof(int));
+	*matrix = (long long*)malloc(rows * columns * sizeof(long long));
 
 	for (size_t i = 0; i < rows; ++i)
 	{
@@ -28,29 +29,41 @@ void generateMatrix(int** matrix, int rows, int columns, int min, int max)
 	}
 }
 
-void readInfoFromFile(int& rows, int& columns, int& min, int& max, int& subrows, int& subcolumns)
+bool readInfoFromFile(int& rows, int& columns, int& min, int& max, int& subrows, int& subcolumns)
 {
 	std::ifstream fin("info.txt");
-	if (!fin) { printf("fail to open info.txt\n"); return; }
+	if (!fin) { printf("fail to open info.txt\n"); return false; }
 
 	fin >> rows >> columns >> min >> max >> subrows >> subcolumns;
 	fin.close();
+	return true;
 }
 
-void writeResultsToFile(int* matrix, int rows, int columns, int subrows, int subcolumns, ExecutionInfo withCuda, ExecutionInfo withoutCuda)
+bool writeResultsToFile(long long* matrix, int rows, int columns, int subrows, int subcolumns, ExecutionInfo withCuda, ExecutionInfo withoutCuda)
 {
 	std::ofstream fout("result.txt");
-	if (!fout) { printf("fail to open result.txt\n"); return; }
+	if (!fout) { printf("fail to open result.txt\n"); return false; }
 
 	auto writeSubmatrix = [&fout, matrix, rows, columns, subrows, subcolumns](Task11::Result res)
 	{
 		int startRow = res.firstElementId / columns;
 		int startColumn = res.firstElementId % columns;
+		int maxSize = 11;
 
 		for (int i = startRow; i < startRow + subrows; i++)
 		{
+			bool skipRows = (subrows > maxSize && (i - startRow) == maxSize / 2);
+
 			for (int j = startColumn; j < startColumn + subcolumns; j++)
-				fout << matrix[i * columns + j] << " ";
+			{
+				bool skipColumn = (subcolumns > maxSize && (j - startColumn) == maxSize / 2);
+
+				fout << (skipRows || skipColumn ? "..." : std::to_string(matrix[i * columns + j])) << " ";
+
+				if (skipColumn) j = startColumn + subcolumns - maxSize / 2 - 1;
+			}
+
+			if (skipRows) i = startRow + subrows - maxSize / 2 - 1;
 
 			fout << "\n";
 		}
@@ -59,26 +72,28 @@ void writeResultsToFile(int* matrix, int rows, int columns, int subrows, int sub
 	fout << "The results are " << (withCuda.res == withoutCuda.res ? "" : "not ") << "equal\n";
 
 	fout << "\nWith cuda:\n" << "Time: " << withCuda.time << "c\n"
-		<< "Sum: " << withCuda.res.sum << "\n" << "Submatrix:\n";
+		 << "Sum: " << withCuda.res.sum << "\n" << "Submatrix:\n";
 	writeSubmatrix(withCuda.res);
 
 	fout << "\nWithout cuda:\n" << "Time: " << withoutCuda.time << "c\n"
-		<< "Sum: " << withoutCuda.res.sum << "\n" << "Submatrix:\n";
+		 << "Sum: " << withoutCuda.res.sum << "\n" << "Submatrix:\n";
 	writeSubmatrix(withoutCuda.res);
 
 	fout.close();
+	return true;
 }
 
 int main(int argc, char** argv)
 {
-	int* matrix;
+	long long* matrix;
 
 	int rows, columns, min, max, subrows, subcolumns;
-	readInfoFromFile(rows, columns, min, max, subrows, subcolumns);
+	if (!readInfoFromFile(rows, columns, min, max, subrows, subcolumns)) return -1;
 
+	printf("Generating matrix...\n");
 	generateMatrix(&matrix, rows, columns, min, max);
 
-	auto execute = [matrix, rows, columns, subrows, subcolumns](Task11::Result(*func)(int*, int,int,int,int)) 
+	auto execute = [matrix, rows, columns, subrows, subcolumns](Task11::Result(*func)(long long*, int,int,int,int))
 	{        
 		ExecutionInfo info;
 		struct timespec start, end;
@@ -89,9 +104,10 @@ int main(int argc, char** argv)
 		return info;
 	};
 
+	printf("Calculation with cuda...\n");
 	ExecutionInfo info1 = execute(Task11::Cuda::findSubmatrixWithMaxSum);
+	printf("Calculation without cuda...\n");
 	ExecutionInfo info2 = execute(Task11::NoCuda::findSubmatrixWithMaxSum);
 
-	writeResultsToFile(matrix, rows, columns, subrows, subcolumns, info1, info2);
-	return 0;
+	return (writeResultsToFile(matrix, rows, columns, subrows, subcolumns, info1, info2) ? 0 : -1);
 }
