dbef52d345e29604044257d2906cdf5f9e3a8130
MaxaiZer <MaxuZerGames@gmail.com>
2023-09-24 17:30:28 +0700

    added a check for digits in name/surname

--- semester7/information-systems-software/lab1/Models/Student.cs
+++ semester7/information-systems-software/lab1/Models/Student.cs
@@ -4,10 +4,12 @@ public class Student
 {
 
     [Required]
+    [RegularExpression(@"[a-zA-Z-]+$", ErrorMessage = "Name: only letters are allowed.")]
     [StringLength(50, MinimumLength = 2)]
     public string Name { get; set; }
 
     [Required]
+    [RegularExpression(@"[a-zA-Z-]+$", ErrorMessage = "Surname: only letters are allowed.")]
     [StringLength(50, MinimumLength = 2)]
     public string Surname { get; set; }
 }
